edc051ee3fd2bb3a4d7f3dd4119dec29bb874011
WyrwaMichal8 <kokojakikoko@gmail.com>
2025-11-25 10:06:22 +0000

    4_1

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
+app.get("/math/circle/:r", (req, res) => {
   const radius = parseFloat(req.params.r);
-  
+
   if (isNaN(radius) || radius < 0) {
-    return res.status(400).json({ error: 'Invalid radius' });
+    return res.status(400).json({ error: "Invalid radius." });
   }
-  
+
   const area = (Math.PI * radius * radius).toFixed(2);
   const circumference = (2 * Math.PI * radius).toFixed(2);
-  
+
   const result = {
     area: area,
-    circumference: circumference
+    circumference: circumference,
   };
-  
+
   res.json(result);
 });
 
 //TODO2
 
-
 //TODO3
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
